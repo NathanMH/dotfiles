6d6bbc4be6672624ce21ab1b732770fc47ca8e5a
Nathan <nathan@musicnate.ca>
2016-04-01 18:49:01 -0400

    Add ability to type , in emacs
    Former-commit-id: bb8bf863cee948e01a303e60acbbda650e052890

--- emacs/init/init-evil-leader.el
+++ emacs/init/init-evil-leader.el
@@ -10,7 +10,9 @@
     "b" 'ibuffer
     "q" 'delete-window
     "tt" 'save-buffer
-    "k" 'kill-buffer-and-window)
+    "k" 'kill-buffer-and-window
+    "m" 'right-char
+    "," 'insert-comma)
 
 (evil-leader/set-key
     "full" 'toggle-frame-fullscreen)
@@ -18,5 +20,15 @@
 ; Nerd-Commenter
 (evil-leader/set-key
     "comm" 'evilnc-comment-or-uncomment-lines)
+
+; Mail
+(evil-leader/set-key
+    "email" 'mu4e
+    "fetch" 'mu4e-get-mail-command)
+
+; Allows me to actually type a comma...TODO ability to do this in insert mode.
+(defun insert-comma ()
+    (interactive)
+    (insert ","))
 
 (provide 'init-evil-leader)
