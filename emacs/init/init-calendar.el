26274bbe738613b841db8b468fdeb5006be83f9e
Nathan <nathan@musicnate.ca>
2016-07-07 19:19:44 -0400

    Change local location of ics to online version.
    Former-commit-id: 48c82873b4923c104956f331a4d6f5b8d9b3e7ee

--- emacs/init/init-calendar.el
+++ emacs/init/init-calendar.el
@@ -6,7 +6,8 @@
 
 ; Settings
 
-	(define-key evil-normal-state-map (kbd ",cal") (lambda () (interactive) (cfw:open-ical-calendar "/home/musicnate/.emacs.d/calendar/emacs-cal.ics")))
+	(define-key evil-normal-state-map (kbd ",cal") (lambda () (interactive) (cfw:open-ical-calendar "https://calendar.google.com/calendar/ical/nathan.mador.house%40gmail.com/public/basic.ics")))
+	; (define-key evil-normal-state-map (kbd ",cal") (lambda () (interactive) (cfw:open-ical-calendar "/home/musicnate/.emacs.d/calendar/emacs-cal.ics")))
 
 	; TODO intergrate org mode
 
