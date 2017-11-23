f10f0a6ec2ddfe9c38e0f559006d30c44378bd8b
NathanMH <nathan@musicnate.ca>
2017-11-22 23:47:46 -0500

    Update colours of emacs tabbar.

--- linux/emacs/init.el
+++ linux/emacs/init.el
@@ -101,12 +101,19 @@
  '(package-selected-packages
    (quote
 	(twittering-mode rainbow-delimiters powerline neotree key-chord flycheck evil-terminal-cursor-changer evil-surround evil-org evil-nerd-commenter evil-leader company-jedi colemak-evil centered-cursor-mode calfw ace-jump-mode)))
- '(send-mail-function nil))
+ '(send-mail-function nil)
+ '(tabbar-separator (quote (0.6))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(tabbar-button ((t (:inherit tabbar-default :foreground "gold"))))
+ '(tabbar-button-highlight ((t (:inherit tabbar-default))))
+ '(tabbar-default ((t (:inherit variable-pitch :background "black" :foreground "#FD971F" :weight bold))))
+ '(tabbar-highlight ((t (:underline t))))
+ '(tabbar-selected ((t (:inherit tabbar-default :background "#090909" :foreground "#F92672"))))
+ '(tabbar-separator ((t (:inherit tabbar-default))))
+ '(tabbar-unselected ((t (:inherit tabbar-default)))))
 
 
--- linux/emacs/init/init-tabbar.el
+++ linux/emacs/init/init-tabbar.el
@@ -17,6 +17,23 @@
 
 (setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
 
+; Look and feel
+
+(setq tabbar-background-color "black")
+
+(custom-set-faces
+ '(tabbar-default ((t (:inherit variable-pitch :background "black" :foreground "#FD971F" :weight bold))))
+ '(tabbar-button ((t (:inherit tabbar-default :foreground "gold"))))
+ '(tabbar-button-highlight ((t (:inherit tabbar-default))))
+
+ '(tabbar-highlight ((t (:underline t))))
+ '(tabbar-selected ((t (:inherit tabbar-default :background "#090909" :foreground "#F92672"))))
+
+ '(tabbar-separator ((t (:inherit tabbar-default))))
+ '(tabbar-unselected ((t (:inherit tabbar-default)))))
+
+;; *** USE THIS TO ADJUST THE SPACES BETWEEN TABS ***
+
 ; Autoload tabbar
 (define-globalized-minor-mode global-tabbar-minor-mode
  tabbar-mode tabbar-mode)
